a835a890a97da5a4d11e4bd41a830059d5019e49
Miz85 <nazim@tuukay.com>
2015-01-19 17:42:55 +0100

    Added venue creation or reuse + event creation or update

--- app.js
+++ app.js
@@ -7,48 +7,56 @@ var config = {
 };
 
 var talks = {
-      'date': '2014-02-12T23:00:00.000Z',
-      'event': {
-      	'name': 'My awesome event',
-      	'date': '2015-02-12T12:00:00.000Z'	
-      },
-      'talks': [{
-        'title': 'Using the API Blueprint to generate nodejs consumers',
-        'speaker': {
-          'twitter': 'bpedro',
-          'name': 'Bruno Pedro',
-          'portrait': 'https://pbs.twimg.com/profile_images/378800000473642830/2f20ecdcd1ec41452b174d04a69e87ee.jpeg'
+  "webhook": {
+    "label": "talk proposal",
+    "title": "Development environments using fig",
+    "level": "Beginner",
+    "language": "en",
+    "speaker": {
+      "twitter": "dias_jorge",
+      "name": "Jorge Dias",
+      "portrait": ""
+    },
+    "description": "Jorge Dias, Software Engineer turned Devops @ Xing, responsible for development and testing environments is going to talk about the research being done at Xing regarding the use of docker and will do a brief overview of working with fig and some custom tools to improve the workflow."
+  },
+  "event": {
+    "date": "2015-02-05T18:00:00.000Z",
+    "name": "BarcelonaJS",
+    "venue": {
+      "name": "Mobile World Center",
+      "address": "C/ Fontanella 2",
+      "private": false
+    },
+    "talks": [
+      {
+        "title": "Serious text editing in the browser",
+        "level": "Advanced",
+        "language": "es",
+        "speaker": {
+          "twitter": "marijnjh",
+          "name": "Marijn Haverbeke",
+          "portrait": ""
         },
-        'level': 'Intermediate',
-        'language': 'en',
-        'git': '',
-        'slides': 'http://www.slideshare.net/bpedro/api-code-generation',
-        'video': 'https://vimeo.com/87488883',
-        'description': 'I\'ll show you how to generate a nodejs API consumer by using the API Blueprint (http://apiblueprint.org/) to generate code from a Postman (http://www.getpostman.com/) collection.'
+        "description": "When a textarea doesn't cut it anymore, there are alternatives: full-featured text and code editors written in browser JavaScript. This talk is about CodeMirror, one such editor. It'll explore the intricacies of faking an editable control, the challenges of making it scale to hundreds of thousands of lines, and the integration of modern code editor features."
       },
       {
-        'title': 'Build stuff using node.js',
-        'speaker': {
-          'twitter': 'nsaouli',
-          'name': 'Nazim Saouli',
-          'portrait': ''
+        "title": "Development environments using fig",
+        "level": "Beginner",
+        "language": "en",
+        "speaker": {
+          "twitter": "dias_jorge",
+          "name": "Jorge Dias",
+          "portrait": ""
         },
-        'level': 'Beginner',
-        'language': 'en',
-        'git': '',
-        'slides': 'http://www.slideshare.net/bpedro/api-code-generation',
-        'video': 'https://vimeo.com/87488883',
-        'description': 'I\'ll discuss all things node.js and how to use it to build cool things.'
+        "description": "Jorge Dias, Software Engineer turned Devops @ Xing, responsible for development and testing environments is going to talk about the research being done at Xing regarding the use of docker and will do a brief overview of working with fig and some custom tools to improve the workflow."
       }
-      ]
-    };
+    ]
+  }
+};
 
 var meetup = require('./meetup')(config);
 
 meetup.trigger(talks, function(err, result){
 	if (err) throw err;
-
-
-	console.log(result);
 });
 // meetup.trigger();
--- meetup.js
+++ meetup.js
@@ -4,16 +4,21 @@ var async = require('async');
 var moment = require('moment');
 
 var STATUS_OK = "ok";
-var STATUS_EDIT = "edit";
-var STATUS_ERROR = "error";
 
 var meetup = function(config) {
 
+  // Check if the configuration is there
   if(!config || !config.meetup.apikey || !config.meetup.groupurl || !config.meetup.groupid) {
       // We should do something
       throw new Error("Missing configuration");
   }
 
+  // Configuration options
+  var _meetupApiKey = config.meetup.apikey;
+  var _meetupGroupId = config.meetup.groupid;
+  var _meetupGroupUrl = config.meetup.groupurl;
+
+  
   // Gets an array of talks and returns a formatted Meetup
   // Event description
   var _getFormattedDescription = function(talks){
@@ -32,76 +37,156 @@ var meetup = function(config) {
     return description;
   }
 
+  // Returns a venue ID to use in the event.
+  // 1/ Will try to find a matching venue in those previously
+  //    used by the meetup group
+  // 2/ If no group venue matches will try to find an open venue that matches
+  // 3/ If no open venue matches, will create one and use it 
+  var _getVenueId = function(venue, cb){
+    // Preparing the API calls
+    var _openVenuesUrl = "http://api.meetup.com/2/open_venues?sign=true&key=" + 
+                          _meetupApiKey + "&country=ES&city=barcelona&order=rating&desc=true&text=" + venue.name;
+    var _venuesUrl = "https://api.meetup.com/" + _meetupGroupUrl + "/venues?sign=true&key=" + _meetupApiKey;
+
+    // Get the list of venues previously used by the group
+     request(_venuesUrl, function (err, response, body){
+        if(!err && response.statusCode == 200){
+          var venues = JSON.parse(body);
+
+          // If the venue exists
+          var matchingGroupVenue = venues.filter(function(venue){
+            return venue.name === talks.event.venue.name;
+          });
+
+          // If no venue matched
+          if(matchingGroupVenue.length == 0) {
+                // No matching Venue so we need to create it.
+                var venueCreationData = {
+                  "address_1": venue.address,
+                  "city": "Barcelona",
+                  "country": "ES",
+                  "name": venue.name
+                };
+
+                request.post(_venuesUrl, {form:venueCreationData}, function(err, response, body){
+                  if(!err && response.statusCode == 200 || response.statusCode == 201){
+                    var venue = JSON.parse(body);
+                    cb(null, venue.id);
+                  }
+                });  
+
+
+          } else {
+            cb(null, matchingGroupVenue[0].id); 
+          }
+        }
+      });
+  };
+
+  // Creates or update a meetup event
+  // Based on the event date specified in the talks object.
+  var _createOrUpdateEvent = function(venueId, talks, cb){
+    // Events Meetup API Calls
+    var _eventsUrl = "https://api.meetup.com/2/events?sign=true&key=" + _meetupApiKey + "&group_id=" + _meetupGroupId;
+    var _createEventUrl = "https://api.meetup.com/2/event?sign=true&key=" + _meetupApiKey;
+
+  
+    // Get all the group meetup events
+    request(_eventsUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+
+        var meetupEvents = JSON.parse(body).results;
+
+        // Check if on of the meetup events matches the event related to the talks
+        var matchingEvent = meetupEvents.filter(function(meetupEvent){
+          return moment(meetupEvent.time).utc().format('x') == moment(talks.event.date).utc().format('x');
+        }); 
+
+        // If the event does not already exist
+        if(matchingEvent.length == 0) {
+          
+          // Prepare the data needed to create the event
+          var eventCreationData = {
+            "group_id": _meetupGroupId, 
+            "group_url": _meetupGroupUrl, 
+            "name": talks.event.name, 
+            "description": _getFormattedDescription(talks.event.talks),
+            "venue_id": venueId,
+            "time": moment(talks.event.date).utc().format('x') // we need to setup a date as a unix ms timestamp or the
+                                                          // event won't be included in the list of existing events
+          };
+
+          // 
+          request.post(_createEventUrl, {form:eventCreationData}, function(err, response, body){
+            if(! err && response.statusCode == 200) {
+              // Event successfully created
+              cb(null, STATUS_OK);
+            } else {
+              cb(err, null);
+            }
+          });
+        } else {
+          // we need to update the event
+          var eventUpdateData = {
+            "group_id": _meetupGroupId,
+            "name": talks.event.name, 
+            "description": _getFormattedDescription(talks.event.talks),
+            "venue_id": venueId,
+            "time": moment(talks.event.date).utc().format('x') // we need to setup a date as a unix ms timestamp or the
+                                                          // event won't be included in the list of existing events
+          };
+
+          // Prepare the API call
+          var _updateEventUrl = "https://api.meetup.com/2/event/"+ matchingEvent[0].id +"?sign=true&key=" + _meetupApiKey;
+
+          // Update the event
+          request.post(_updateEventUrl, {form:eventUpdateData}, function (err, response, body){
+            if(! err && response.statusCode == 200) {
+              // Event successfully updated
+              cb(null, STATUS_OK);
+            } else {
+              cb(err, null);
+            }
+          });
+        }
+
+      } else {
+        cb(error, null);
+      }
+    });
+  };
+
 
   return {
     trigger :function(talks, cb) {
 
-      if(!talks) {
-        // we should do something as well
+      if(!talks.event || !talks.event.talks) {
+        // Missing Event or missing list of talks
+        cb("Missing Event or missing list of talks", null);
       }
 
-      var meetupApiKey = config.meetup.apikey;
-      var meetupGroupId = config.meetup.groupid;
-      var meetupGroupUrl = config.meetup.groupurl;
-
-      // Get the group ID
-      var eventsUrl = "https://api.meetup.com/2/events?sign=true&key=" + meetupApiKey + "&group_id=" + meetupGroupId;
-      var createEventUrl = "https://api.meetup.com/2/event?sign=true&key=" + meetupApiKey;
-
       async.waterfall([
-        // Check if the event related to the talk already exists in meetup
+        // Check if the venue exists
         function(callback) {
-          // Get all the group meetup events
-          request(eventsUrl, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-
-              var meetupEvents = JSON.parse(body).results;
-
-              // Check if on of the meetup events matches the event related to the talks
-              var matchingEvent = meetupEvents.filter(function(meetupEvent){
-                return meetupEvent.name.toLowerCase() === talks.event.name.toLowerCase();
-              }); 
-              callback(null, matchingEvent);
-
-            } else {
-              callback(error, null);
-            }
-          });
-        },
-        function(matchingEvent, callback) {
-          // If the event does not already exist
-          if(matchingEvent.length == 0) {
-            
-            // We need to create it
-            var formData = {
-              "group_id": meetupGroupId, 
-              "group_url": meetupGroupUrl, 
-              "name": talks.event.name, 
-              "description": _getFormattedDescription(talks.talks),
-              "time": moment(talks.event.date).format('x') // we need to setup a date as a unix ms timestamp or the
-                                                            // event won't be included in the list of existing events
-            };
-
-            // TODO : Handle venue, and other event details.
-
-            request.post(createEventUrl, {form:formData}, function(err, response, body){
-              if(! err && response.statusCode == 200) {
-                // Event successfully created
-                callback(null, STATUS_OK);
-              } else {
+           _getVenueId(talks.event.venue, function(err, venueId){
+              if(err) {
                 callback(err, null);
+              } else {
+                callback(null, venueId);
               }
-            });
-          } else {
-            // we need to update the event
-            callback(null, STATUS_EDIT);
-          }
+           });
         },
-        function(status, callback) {
-          // If we need to update an existing event.
-          
-        }
 
+        // Check if the event related to the talk already exists in meetup
+        function(venueId,callback) {
+          _createOrUpdateEvent(venueId, talks, function(err, status){
+              if(err) {
+                callback(err, null);
+              } else {
+                callback(null, status);
+              }
+          });
+        }
       ], function(err, result){
           cb(err, result);
       });
